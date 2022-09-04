8446cdbf6fd291377147037f89b843e7f8c867dc
skydreamer21 <skydreamer210@gmail.com>
2022-09-04 20:47:57 +0900

    [Feat] submit시 Form칸 없애고 인사 보여주기

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.dir(event);
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.add("appear");
+
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
