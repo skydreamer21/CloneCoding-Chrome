5c517d4faed5efa9a1b14b0908cd12caa2cf94d4
skydreamer21 <skydreamer210@gmail.com>
2022-09-06 16:26:01 +0900

    [Feat] #4.5, #4.6 로컬스토리지에 username 저장하고 보여주기

--- app.js
+++ app.js
@@ -2,14 +2,31 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    loginForm.classList.add("hidden");
-    greeting.innerText = `Hello ${username}!`;
-    greeting.classList.add("appear");
-
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}!`;
+    greeting.classList.add("appear");
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+    // show form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
+
+
+
