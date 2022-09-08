264034579dfe0df08c41ae1024ada433c4cf362b
skydreamer21 <skydreamer210@gmail.com>
2022-09-08 21:05:08 +0900

    [Fix] 시계 am/pm 로 변경

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,15 @@ const clock = document.querySelector("#clock");
 
 function updateClock() {
     const date = new Date();
-    const hours = date.getHours().toString().padStart(2,"0");
+    let hours = date.getHours();
+    const AMPMInfo = hours >= 12 ? "pm" : "am";
+    if (AMPMInfo === "pm" && hours !== 12) {
+        hours -= 12;
+    }
+    hours = hours.toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2,"0");;
     const seconds = date.getSeconds().toString().padStart(2,"0");;
-    const time = `${hours}:${minutes}:${seconds}`;
+    const time = `${AMPMInfo} ${hours}:${minutes}:${seconds}`;
     clock.innerText = time; 
 }
 
