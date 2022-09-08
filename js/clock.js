a3219e60da9084931baf9f331ddbf2dacb6f086e
skydreamer21 <skydreamer210@gmail.com>
2022-09-08 18:35:33 +0900

    [Fix] 시계 각 요소 2자리로 출력
    padStart 사용

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,11 @@ const clock = document.querySelector("#clock");
 
 function updateClock() {
     const date = new Date();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
-    const time = `${hour}:${minute}:${second}`;
-    clock.innerText = time;
+    const hours = date.getHours().toString().padStart(2,"0");
+    const minutes = date.getMinutes().toString().padStart(2,"0");;
+    const seconds = date.getSeconds().toString().padStart(2,"0");;
+    const time = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = time; 
 }
 
 updateClock(); // 없으면 시간이 표시되는 1초까지는 00:00:00 으로 나온다
