84be6f1d52e1cd778c246d237faebe1e2d8f6901
skydreamer21 <skydreamer210@gmail.com>
2022-09-09 21:00:01 +0900

    [Feat] todoList(2) - todoList 삭제 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,28 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoUl = document.querySelector("#todo");
 
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(todo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    span.innerText = todo;
+    deleteButton.innerText = "❌";
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click", deleteTodo);
+    todoUl.appendChild(li);
+}
+
 function handleSubmitTodoList(event) {
     event.preventDefault();
     const todo = todoInput.value;
     todoInput.value = "";
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    li.appendChild(span);
-    span.innerText = todo;
-    todoUl.appendChild(li);
+    paintTodo(todo);
 }
 
 todoForm.addEventListener("submit", handleSubmitTodoList);
