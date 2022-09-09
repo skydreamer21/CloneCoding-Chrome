c6257832988717e247085156c41eb2b638ed71b1
skydreamer21 <skydreamer210@gmail.com>
2022-09-09 16:53:09 +0900

    [Feat] 랜덤 배경 기능 추가

---
+++ js/background.js
@@ -0,0 +1,24 @@
+import { readByTxt } from "./util.js";
+
+function getBackgroundImgList(dirPathInfo) {
+    const imagePathList = dirPathInfo.match(/\/image\/background\/bg[0-9].(jpg|jpeg|png)/g);
+    const imageList = imagePathList.map((path) => {
+        const splitIdx = path.lastIndexOf("/");
+        return path.substring(splitIdx+1);
+    });
+    return imageList;
+}
+
+const dirPath = "../image/background/"
+const dirPathInfo = readByTxt(dirPath);
+const bgList = getBackgroundImgList(dirPathInfo);
+console.log(bgList);
+
+const numOfBg = bgList.length;
+const randomBgIndex = Math.floor(Math.random()*numOfBg);
+
+const bgImg = document.createElement("img");
+bgImg.src = dirPath + bgList[randomBgIndex];
+
+const body = document.querySelector("body");
+body.appendChild(bgImg);
